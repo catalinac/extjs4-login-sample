e1c51f801b0661c3bf3b2ea239a239d06e7adaee
acoatu <acoatu@pentalog.fr>
2015-09-10 19:02:25 +0300

    - primul exemplu:
    A panel with a title bar containing the text "Hello Ext" and the "welcome" message in the panel's body area.

--- public/app.js
+++ public/app.js
@@ -1,39 +1,56 @@
-var strings = {};
-strings.nl = {
-  "_Username": "Gebruikersnaam" 
-};
-strings.en = {
-  "_Username": "Username" 
-};
+//var strings = {};
+//strings.nl = {
+//  "_Username": "Gebruikersnaam" 
+//};
+//strings.en = {
+//  "_Username": "Username" 
+//};
+//
+//String.prototype.l = function () {
+//  console.log('name: ' + Ext.app.Application.name);
+//  var lang = 'nl';
+//  var result = this;
+//  if (strings[lang] && strings[lang][this])
+//    result = strings[lang][this];
+//  return result;
+//};
+//
+//Ext.application({
+//    name: 'LM',
+//
+//    appFolder: 'app',
+//
+//    controllers: [
+//        'LoginController'
+//    ],
+//    
+//    launch: function() {
+//			//Ext.create('LM.view.Main', {});
+//     	Ext.create('LM.view.LoginWindow', {}).show();
+//			Ext.create('Ext.container.Viewport', {
+//          layout: 'fit',
+//          items: [
+//              {
+//                  xtype: 'mainview',
+//              }
+//          ]
+//			});
+//    },
+//    //autoCreateViewport: true
+//});
 
-String.prototype.l = function() {
-  console.log('name: '+Ext.app.Application.name);
-  var lang = 'nl';
-  var result = this;
-  if (strings[lang] && strings[lang][this]) result = strings[lang][this]; 
-  return result;
-};
-
+Ext.require('Ext.container.Viewport');
 Ext.application({
-    name: 'LM',
-
-    appFolder: 'app',
-
-    controllers: [
-        'LoginController'
-    ],
-    
-    launch: function() {
-			//Ext.create('LM.view.Main', {});
-     	Ext.create('LM.view.LoginWindow', {}).show();
-			Ext.create('Ext.container.Viewport', {
-          layout: 'fit',
-          items: [
-              {
-                  xtype: 'mainview',
-              }
-          ]
-			});
-    },
-    //autoCreateViewport: true
-});
+  name: 'HelloExt',
+  launch: function () {
+    Ext.create('Ext.container.Viewport', {
+      layout: 'fit',
+      items: [
+        {
+          title: 'Hello Ext',
+          html: 'Hello! Welcome to Ext JS.'
+        }
+      ]
+    });
+  }
+}); //You should see a panel with a title bar containing the text "Hello Ext" and the "welcome" message in the panel's body area.
